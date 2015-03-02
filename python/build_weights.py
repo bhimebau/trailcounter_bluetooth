e721cc0ed22183d8ed0bfd4b807f0f6b0f08e0a0
Bryce Himebaugh <bhimebau@gmail.com>
2015-03-02 17:30:32 -0500

    Reading weight data from the matlab files

--- python/build_weights.py
+++ python/build_weights.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 
-print "hello"
+import sys
+import scipy.io
+
+matdata = scipy.io.loadmat(sys.argv[1])
+weightsIW = matdata['weightsIW']
+unit_num = int(sys.argv[2]) - 1 
+print weightsIW[0,unit_num]
+
