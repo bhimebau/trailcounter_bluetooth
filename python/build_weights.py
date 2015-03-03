90d09f15bbaeedc25011decf0ad487da41254eff
Bryce Himebaugh <bhimebau@gmail.com>
2015-03-03 05:04:49 -0500

    Working on the weight translator.

--- python/build_weights.py
+++ python/build_weights.py
@@ -4,7 +4,18 @@ import sys
 import scipy.io
 
 matdata = scipy.io.loadmat(sys.argv[1])
-weightsIW = matdata['weightsIW']
 unit_num = int(sys.argv[2]) - 1 
-print weightsIW[0,unit_num]
+
+weightsIW = matdata['weightsIW']
+weightsLW = matdata['weightsLW']
+
+weights_ih = weightsIW[0,unit_num]
+weights_ho = weightsLW[0,unit_num]
+
+print weights_ih[0,0]
+print weights_ho[0,0]
+
+
+
+
 
