5613cdb80db154c7df10db86648d6898310e4455
Bryce Himebaugh <bhimebau@gmail.com>
2015-03-03 15:15:32 -0500

    Working parser and array generator. Need file creation.

--- python/build_weights.py
+++ python/build_weights.py
@@ -15,20 +15,40 @@ weights_ho = weightsLW[0,unit_num]
 (hidden_neurons, hidden_neuron_inputs) = weights_ih.shape
 (output_neurons, output_neuron_inputs) = weights_ho.shape
 
-print weights_ih
 print "float weights_ih[%d][%d] = { \\" %(hidden_neuron_inputs,hidden_neurons)
-count = 0;
+row_count = 0;
+total_count = 0;
 for neuron in range(hidden_neurons):
     for ninput in range(hidden_neuron_inputs):
-        if (count < 3):
-            print "%+2.10Le,"%(weights_ih[neuron,ninput]),
-            count = count + 1
+        if (total_count < (hidden_neuron_inputs * hidden_neurons - 1)):
+            if (row_count < 3):
+                print "%+2.10Le,"%(weights_ih[neuron,ninput]),
+                row_count = row_count + 1
+            else:
+                print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
+                row_count = 0
+            total_count = total_count + 1
         else:
-            print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
-            count = 0
-print "};"
+            print "%+2.10Le};"%(weights_ih[neuron,ninput])
 
-print hidden_neurons, hidden_neuron_inputs, output_neurons, output_neuron_inputs 
+print "float weights_ho[%d][%d] = { \\" %(output_neuron_inputs,output_neurons)
+row_count = 0;
+total_count = 0;
+for neuron in range(output_neurons):
+    for ninput in range(output_neuron_inputs):
+        if (total_count < (output_neuron_inputs * output_neurons - 1)):
+            if (row_count < 3):
+                print "%+2.10Le,"%(weights_ih[neuron,ninput]),
+                row_count = row_count + 1
+            else:
+                print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
+                row_count = 0
+            total_count = total_count + 1
+        else:
+            print "%+2.10Le};"%(weights_ih[neuron,ninput])
+
+
+# print hidden_neurons, hidden_neuron_inputs, output_neurons, output_neuron_inputs 
 
 
 
