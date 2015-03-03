4cdb01b5c0b042a56219642c3577e3a686010adb
Bryce Himebaugh <bhimebau@gmail.com>
2015-03-03 15:32:11 -0500

    Working version that generates weight.h file with the associated array of weights for the neural architecture

--- python/build_weights.py
+++ python/build_weights.py
@@ -3,8 +3,10 @@
 import sys
 import scipy.io
 
+
 matdata = scipy.io.loadmat(sys.argv[1])
 unit_num = int(sys.argv[2]) - 1 
+include_file=open(sys.argv[3], 'w+')
 
 weightsIW = matdata['weightsIW']
 weightsLW = matdata['weightsLW']
@@ -15,40 +17,50 @@ weights_ho = weightsLW[0,unit_num]
 (hidden_neurons, hidden_neuron_inputs) = weights_ih.shape
 (output_neurons, output_neuron_inputs) = weights_ho.shape
 
-print "float weights_ih[%d][%d] = { \\" %(hidden_neuron_inputs,hidden_neurons)
+print >>include_file, "#define IH_INPUTS %d" % (hidden_neuron_inputs) 
+print >>include_file, "#define IH_NEURONS %d" % (hidden_neurons) 
+print >>include_file, "float weights_ih[IH_INPUTS][IH_NEURONS] = {\\"
 row_count = 0;
 total_count = 0;
 for neuron in range(hidden_neurons):
     for ninput in range(hidden_neuron_inputs):
         if (total_count < (hidden_neuron_inputs * hidden_neurons - 1)):
-            if (row_count < 3):
-                print "%+2.10Le,"%(weights_ih[neuron,ninput]),
+            if (row_count == 0):
+                print >>include_file, "   %+2.10Le,"%(weights_ih[neuron,ninput]),
+                row_count = row_count + 1
+            elif (row_count < 3):
+                print >>include_file, "%+2.10Le,"%(weights_ih[neuron,ninput]),
                 row_count = row_count + 1
             else:
-                print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
+                print >>include_file, "%+2.10Le,\\"%(weights_ih[neuron,ninput])
                 row_count = 0
             total_count = total_count + 1
         else:
-            print "%+2.10Le};"%(weights_ih[neuron,ninput])
+            print >>include_file, "%+2.10Le};"%(weights_ih[neuron,ninput])
 
-print "float weights_ho[%d][%d] = { \\" %(output_neuron_inputs,output_neurons)
+
+print >>include_file, "#define HO_INPUTS %d" % (output_neuron_inputs) 
+print >>include_file, "#define HO_NEURONS %d" % (output_neurons) 
+print >>include_file, "float weights_ho[HO_INPUTS][HO_NEURONS] = {\\"
 row_count = 0;
 total_count = 0;
 for neuron in range(output_neurons):
     for ninput in range(output_neuron_inputs):
         if (total_count < (output_neuron_inputs * output_neurons - 1)):
-            if (row_count < 3):
-                print "%+2.10Le,"%(weights_ih[neuron,ninput]),
+            if (row_count == 0):
+                print >>include_file, "   %+2.10Le,"%(weights_ih[neuron,ninput]),
+                row_count = row_count + 1
+            elif (row_count < 3):
+                print >>include_file, "%+2.10Le,"%(weights_ih[neuron,ninput]),
                 row_count = row_count + 1
             else:
-                print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
+                print >>include_file, "%+2.10Le,\\"%(weights_ih[neuron,ninput])
                 row_count = 0
             total_count = total_count + 1
         else:
-            print "%+2.10Le};"%(weights_ih[neuron,ninput])
+            print >>include_file, "%+2.10Le};"%(weights_ih[neuron,ninput])
 
 
-# print hidden_neurons, hidden_neuron_inputs, output_neurons, output_neuron_inputs 
 
 
 
