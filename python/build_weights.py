ea3ca676fc3c10fb89944eeebc6b7ebc19bafc40
Bryce Himebaugh <bhimebau@gmail.com>
2015-03-03 11:30:10 -0500

    checkpoint on code to create weights include file

--- python/build_weights.py
+++ python/build_weights.py
@@ -12,10 +12,23 @@ weightsLW = matdata['weightsLW']
 weights_ih = weightsIW[0,unit_num]
 weights_ho = weightsLW[0,unit_num]
 
-print weights_ih[0,0]
-print weights_ho[0,0]
+(hidden_neurons, hidden_neuron_inputs) = weights_ih.shape
+(output_neurons, output_neuron_inputs) = weights_ho.shape
 
+print weights_ih
+print "float weights_ih[%d][%d] = { \\" %(hidden_neuron_inputs,hidden_neurons)
+count = 0;
+for neuron in range(hidden_neurons):
+    for ninput in range(hidden_neuron_inputs):
+        if (count < 3):
+            print "%+2.10Le,"%(weights_ih[neuron,ninput]),
+            count = count + 1
+        else:
+            print "%+2.10Le,\\"%(weights_ih[neuron,ninput])
+            count = 0
+print "};"
 
+print hidden_neurons, hidden_neuron_inputs, output_neurons, output_neuron_inputs 
 
 
 
