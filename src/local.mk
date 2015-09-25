1215594edcac74492c8106c5c7d2925cb59f173e
Bryce Himebaugh <bhimebau@indiana.edu>
2015-09-25 16:14:56 -0400

    cleaned out unused ST modules

--- src/local.mk
+++ src/local.mk
@@ -3,8 +3,9 @@
 download: $(BUILDDIR)/$(PROJECT).bin                                            
 	st-flash write $(BUILDDIR)/$(PROJECT).bin 0x8000000 > st-flash.log 2>&1    
 
-etags:                                                                          
+etags:                                                                         	
 	find $(SRCPATHS) -type f -iname "*.[ch]" | xargs etags --append         
 	find $(INCDIR) -type f -iname "*.[ch]" | xargs etags --append           
 	find $(DINCDIR) -type f -iname "*.[ch]" | xargs etags --append          
 	find $(UINCDIR) -type f -iname "*.[ch]" | xargs etags --append 
+	find ../STM32F30x_StdPeriph_Driver -type f -iname "*.[ch]" | xargs etags --append 
