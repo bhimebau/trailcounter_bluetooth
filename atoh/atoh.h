43aa6360b6e64fafc89790c790156113ee3a5e48
Bryce Himebaugh <bhimebau@gmail.com>
2015-10-01 20:42:16 -0400

    Added ascii to hex conversion function to allow hex to be input for the accel experimentaion.

---
+++ atoh/atoh.h
@@ -0,0 +1,39 @@
+/* atoh.h --- 
+ * 
+ * Filename: atoh.h
+ * Description: 
+ * Author: Bryce Himebaugh
+ * Maintainer: 
+ * Created: Mon Feb  2 10:45:39 2015
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change log:
+ * 
+ * 
+ */
+
+/* Copyright (c) 2004-2007 The Trustees of Indiana University and 
+ * Indiana University Research and Technology Corporation.  
+ * 
+ * All rights reserved. 
+ * 
+ * Additional copyrights may follow 
+ */
+
+/* Code: */
+
+int atoh(char *, int *);
+
+/* atoh.h ends here */
--- src/adxl362.c
+++ src/adxl362.c
@@ -38,7 +38,9 @@
 #include "hal.h"
 #include "adxl362.h"
 #include "chprintf.h"
+#include <atoh.h>
 #include <stdlib.h>
+
 
 /* SPI configuration, sets up PortA Bit 8 as the chip select for the adxl362 */
 static SPIConfig adxl362_cfg = {
@@ -56,9 +58,8 @@ uint8_t adxl362_read_register (uint8_t address) {
 
   spiStart(&SPID1, &adxl362_cfg);       /* Setup transfer parameters. */
   spiSelect(&SPID1);                    /* Slave Select assertion. */
-  spiSend(&SPID1, 1, &command);             /* Transmit Read Command */
-  spiSend(&SPID1, 1, &address);          /* Read Command */
-  //  spiSend(&SPID1, 1, &dummy);             /* Dummy byte to generate clocks */
+  spiSend(&SPID1, 1, &command);         /* Transmit Read Command */
+  spiSend(&SPID1, 1, &address);         /* Read Command */
   spiReceive(&SPID1, 1, &receive_data); /* Read the data that is returned */
   spiUnselect(&SPID1);                  /* Slave Select de-assertion. */
   return (receive_data);
@@ -69,10 +70,10 @@ void adxl362_write_register (uint16_t address, uint8_t data) {
 
   spiStart(&SPID1, &adxl362_cfg);      /* Setup transfer parameters. */
   spiSelect(&SPID1);                   /* Slave Select assertion.    */
-  spiSend(&SPID1, 1, &command);            /* Write Command              */
-  spiSend(&SPID1, 1, &address);        /* Address */ 
-  spiSend(&SPID1, 1, &data);           /* Data    */
-  spiUnselect(&SPID1);                 /* Slave Select de-assertion.       */
+  spiSend(&SPID1, 1, &command);        /* Write Command              */
+  spiSend(&SPID1, 1, &address);        /* Address                    */ 
+  spiSend(&SPID1, 1, &data);           /* Data                       */
+  spiUnselect(&SPID1);                 /* Slave Select de-assertion. */
 }
 
 void adxl362_init (void) {
@@ -88,7 +89,7 @@ void cmd_adxl362_read(BaseSequentialStream *chp, int argc, char *argv[]) {
   (void)argv;
   int address = 0x00;
   if (argc == 1) {
-    address=atoi(argv[0]);
+    atoh(argv[0],&address);
     chprintf(chp,"0x%02x\n\r",adxl362_read_register(address));
   }
   else {
@@ -101,8 +102,8 @@ void cmd_adxl362_write(BaseSequentialStream *chp, int argc, char *argv[]) {
   int address = 0x00;
   int data = 0x00;
   if (argc == 2) {
-    address=atoi(argv[0]);
-    data=atoi(argv[1]);
+    atoh(argv[0],&address);
+    atoh(argv[1],&data);
     adxl362_write_register((uint16_t) address, (uint8_t) data);
   }
   else {
